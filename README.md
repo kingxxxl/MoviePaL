7dbb019d6b478110ee1caae9a61184a473931c0b
kingxxxl <103191276+kingxxxl@users.noreply.github.com>
2022-06-11 18:54:17 +0300

    + README.md

--- README.md
+++ README.md
@@ -23,3 +23,38 @@
 
 # Flow
 ![](MoviePaL-Flow.svg)
+
+# Herokuapp
+
+
+Register using  - `<POST>` : <https://moviepal1.herokuapp.com/user/register>
+
+Find a movie using its name - `<GET>` : <https://moviepal1.herokuapp.com/movie/lookby/name/{movie-name}>
+
+Find a movie using its id - `<GET>` : <https://moviepal1.herokuapp.com/movie/lookby/id/{movie-name}>
+
+Search for movies using the name - `<GET>` : <https://moviepal1.herokuapp.com/movie/lookby/search/name/{movie-name}>
+
+
+Add movie to your wish list using the name  - `<POST>` : <https://moviepal1.herokuapp.com/movie/add/name/wishlist/{movie-name}>
+
+Add movie to your favorite list using the name  - `<POST>` : <https://moviepal1.herokuapp.com/movie/add/name/favorite-list/{movie-name}>
+
+Add movie to your watched list using the name  - `<POST>` : <https://moviepal1.herokuapp.com/movie/add/name/watched-list/{movie-name}>
+
+Remove movie to your wish list using the name  - `<DELETE>` : <https://moviepal1.herokuapp.com/movie/remove/name/wishlist/{movie-name}>
+
+Remove movie to your favorite list using the name  - `<DELETE>` : <https://moviepal1.herokuapp.com/movie/remove/name/favorite-list/{movie-name}>
+
+Remove movie to your watched list using the name  - `<DELETE>` : <https://moviepal1.herokuapp.com/movie/remove/name/watched-list/{movie-name}>
+
+
+To show all movie in a wish list
+`<GET>` : <https://moviepal1.herokuapp.com/user/wishlist>
+
+To show all movie in a favorite list
+`<GET>` : <https://moviepal1.herokuapp.com/user/favorite-list>
+
+To show all movie in a watched list
+`<GET>` : <https://moviepal1.herokuapp.com/user/watched-list>
+
--- src/main/java/com/example/moviepal/Seedrs/Seed.java
+++ src/main/java/com/example/moviepal/Seedrs/Seed.java
@@ -1,69 +1,69 @@
-package com.example.moviepal.Seedrs;
-
-import com.example.moviepal.model.User;
-import com.example.moviepal.model.WishListMovie;
-import com.example.moviepal.repository.UserRepository;
-import com.example.moviepal.repository.WishListMovieRepository;
-import com.example.moviepal.service.MovieService;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Component
-@RequiredArgsConstructor
-public class Seed implements CommandLineRunner {
-    Logger logger = LoggerFactory.getLogger(Seed.class);
-    private final UserRepository userRepository;
-    private final WishListMovieRepository wishListMovieRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-        loadUserData();
-    }
-    private void loadUserData() {
-        logger.info("starting loadUserData from Seed");
-        List<String> movies = new ArrayList<>();
-        movies.add("tt2446980");
-        movies.add("tt0206314");
-        movies.add("tt0107282");
-        if (userRepository.count() == 0) {
-            for (int i = 0; i < 3; i++) {
-
-                User user = new User();
-                user.setUsername("TEMP"+i);
-                user.setPassword("a1234567a"+i);
-                String hashedPassword=new BCryptPasswordEncoder().encode(user.getPassword());
-                user.setPassword(hashedPassword);
-                user.setRole("Admin");
-
-                userRepository.save(user);
-
-//                WishListMovie wishListMovie = new WishListMovie();
-//                wishListMovie.setUser(user);
-//                wishListMovie.setListId(i);
-//                wishListMovie.setMovieId(movies.get(i));
-//                System.out.println("wish is:"+wishListMovie);
+//package com.example.moviepal.Seedrs;
 //
-//                wishListMovieRepository.save(wishListMovie);
-            }
-            logger.info("Seed completed: number of rows added: " + userRepository.count());
-        }
-        User user = new User();
-        user.setId(201);
-        user.setUsername("TEMP"+201);
-        user.setPassword("a1234567a"+201);
-        String hashedPassword=new BCryptPasswordEncoder().encode(user.getPassword());
-        user.setPassword(hashedPassword);
-        user.setRole("User");
-
-        userRepository.save(user);
-        logger.info("Seed completed: number of row added: " + 1);
-
-    }
-}
+//import com.example.moviepal.model.User;
+//import com.example.moviepal.model.WishListMovie;
+//import com.example.moviepal.repository.UserRepository;
+//import com.example.moviepal.repository.WishListMovieRepository;
+//import com.example.moviepal.service.MovieService;
+//import lombok.RequiredArgsConstructor;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class Seed implements CommandLineRunner {
+//    Logger logger = LoggerFactory.getLogger(Seed.class);
+//    private final UserRepository userRepository;
+//    private final WishListMovieRepository wishListMovieRepository;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        loadUserData();
+//    }
+//    private void loadUserData() {
+//        logger.info("starting loadUserData from Seed");
+//        List<String> movies = new ArrayList<>();
+//        movies.add("tt2446980");
+//        movies.add("tt0206314");
+//        movies.add("tt0107282");
+//        if (userRepository.count() == 0) {
+//            for (int i = 0; i < 3; i++) {
+//
+//                User user = new User();
+//                user.setUsername("TEMP"+i);
+//                user.setPassword("a1234567a"+i);
+//                String hashedPassword=new BCryptPasswordEncoder().encode(user.getPassword());
+//                user.setPassword(hashedPassword);
+//                user.setRole("Admin");
+//
+//                userRepository.save(user);
+//
+////                WishListMovie wishListMovie = new WishListMovie();
+////                wishListMovie.setUser(user);
+////                wishListMovie.setListId(i);
+////                wishListMovie.setMovieId(movies.get(i));
+////                System.out.println("wish is:"+wishListMovie);
+////
+////                wishListMovieRepository.save(wishListMovie);
+//            }
+//            logger.info("Seed completed: number of rows added: " + userRepository.count());
+//        }
+//        User user = new User();
+//        user.setId(201);
+//        user.setUsername("TEMP"+201);
+//        user.setPassword("a1234567a"+201);
+//        String hashedPassword=new BCryptPasswordEncoder().encode(user.getPassword());
+//        user.setPassword(hashedPassword);
+//        user.setRole("User");
+//
+//        userRepository.save(user);
+//        logger.info("Seed completed: number of row added: " + 1);
+//
+//    }
+//}
