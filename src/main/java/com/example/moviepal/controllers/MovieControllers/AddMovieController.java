ca1c9378ccd9f5416a7bee1e9c9668bfb818c7a9
kingxxxl <103191276+kingxxxl@users.noreply.github.com>
2022-06-12 08:51:00 +0300

    + fixed authority access

--- src/main/java/com/example/moviepal/controllers/MovieControllers/AddMovieController.java
+++ src/main/java/com/example/moviepal/controllers/MovieControllers/AddMovieController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/movie/add")
+@RequestMapping("movie/add")
 @RequiredArgsConstructor
 public class AddMovieController {
 
